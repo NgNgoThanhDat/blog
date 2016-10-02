7c60d05b3736258c2ccaa00594af87779f5ed0dd
Dash Nguyen <datthanh.9982@gmail.com>
2016-10-03 03:27:34 +0700

    [Dat] - Update lan 3

--- config/application.rb
+++ config/application.rb
@@ -11,11 +11,11 @@ module Blog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.assets.version = '1.0'
-    config.assets.initialize_on_precompile = false
+ #    config.assets.version = '1.0'
+ #    config.assets.initialize_on_precompile = false
 
 
-	config.assets.raise_runtime_errors = false
-	config.assets.unknown_asset_fallback = false
+	# config.assets.raise_runtime_errors = false
+	# config.assets.unknown_asset_fallback = false
   end
 end
